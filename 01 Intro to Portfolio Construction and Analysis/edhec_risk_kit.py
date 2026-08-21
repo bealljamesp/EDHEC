2657bc515ccc1ac60a079e57c871e3b7d4326d52
James Beall <bealljamesp@gmail.com>
2026-08-20 18:48:52 -0700

    Add new Jupyter notebook for module 02 in portfolio construction and analysis

--- 01 Intro to Portfolio Construction and Analysis/edhec_risk_kit.py
+++ 01 Intro to Portfolio Construction and Analysis/edhec_risk_kit.py
@@ -157,7 +157,7 @@ def cvar_historic(r, level=5):
         raise TypeError("Expected r to be Series or DataFrame")
 
 
-def annualize_vol(r, periods_per_year):
+def annualized_vol(r, periods_per_year):
     """
     Annualizes the vol of a set of returns
     """
@@ -169,8 +169,17 @@ def sharpe_ratio(r, risk_free_rate, periods_per_year):
     Computes the annualized Sharpe ratio of a set of returns
     """
     # Convert the annual riskfree rate to per period
-    rf_per_period = (1 + riskfree_rate) ** (1 / periods_per_year) - 1
+    rf_per_period = (1 + risk_free_rate) ** (1 / periods_per_year) - 1
     excess_ret = r - rf_per_period
     ann_ex_ret = excess_ret.mean() * periods_per_year
     ann_vol = annualize_vol(r, periods_per_year)
     return ann_ex_ret / ann_vol
+
+
+def annualized_rets(r, periods_per_year):
+    """
+    Annualizes a set of returns
+    """
+    compounded_growth = (1 + r).prod()
+    n_periods = r.shape[0]
+    return compounded_growth ** (periods_per_year / n_periods) - 1
