3a6b15f5163eb16f1a3e38695df0fb11e4ffe056
James Beall <bealljamesp@gmail.com>
2026-08-20 18:38:15 -0700

    Refactor portfolio analysis code: remove old edhec_risk_kit.py, add new implementation with enhanced functions; update Jupyter notebook for improved data handling and visualization; create LAB notebook for system architecture and performance profiling.

--- 01 Intro to Portfolio Construction and Analysis/edhec_risk_kit.py
+++ 01 Intro to Portfolio Construction and Analysis/edhec_risk_kit.py
@@ -5,7 +5,8 @@ from scipy.stats import norm
 
 def drawdown(return_series: pd.Series):
     """
-    Takes a time series of asset returns and outputs a DataFrame with columns for the wealth index, previous peaks, and percentage drawdown."""
+    Takes a time series of asset returns and outputs a DataFrame with columns for the wealth index, previous peaks, and percentage drawdown.
+    """
     wealth_index = 1000 * (1 + return_series).cumprod()
     previous_peaks = wealth_index.cummax()
     drawdown = (wealth_index - previous_peaks) / previous_peaks
@@ -42,6 +43,23 @@ def get_hfi_returns():
     )
     hfi = hfi / 100
     return hfi
+
+
+def get_ind_returns():
+    """
+    Load and format the Ken French 30 Industry Portfolios Value Weighted Monthly Returns.
+    """
+    ind = pd.read_csv(
+        "../data/ind30_m_vw_rets.csv",
+        header=0,
+        index_col=0,
+        parse_dates=True,
+        na_values=-99.99,
+    )
+    ind = ind / 100
+    ind.index = pd.to_datetime(ind.index, format="%Y%m").to_period("M")
+    ind.columns = ind.columns.str.strip()
+    return ind
 
 
 def semideviation_0(r):
@@ -137,3 +155,22 @@ def cvar_historic(r, level=5):
         return r.apply(cvar_historic, level=level)
     else:
         raise TypeError("Expected r to be Series or DataFrame")
+
+
+def annualize_vol(r, periods_per_year):
+    """
+    Annualizes the vol of a set of returns
+    """
+    return r.std() * (periods_per_year**0.5)
+
+
+def sharpe_ratio(r, risk_free_rate, periods_per_year):
+    """
+    Computes the annualized Sharpe ratio of a set of returns
+    """
+    # Convert the annual riskfree rate to per period
+    rf_per_period = (1 + riskfree_rate) ** (1 / periods_per_year) - 1
+    excess_ret = r - rf_per_period
+    ann_ex_ret = excess_ret.mean() * periods_per_year
+    ann_vol = annualize_vol(r, periods_per_year)
+    return ann_ex_ret / ann_vol
